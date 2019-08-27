065581ddf49fd33874a4294f1ad420387c2aaff5
minhnvgch17079 <minhnvgch17079@fpt.edu.vn>
2019-08-27 21:11:14 +0700

    asd

--- pg.js
+++ pg.js
@@ -1,10 +1,10 @@
 const {Client, Pool} = require('pg')
 const client = new Client({
-    database: 'xuyen',
-    host: 'localhost',
-    user: 'postgres',
-    password: '2108',
-    port: 5433
+    database: 'd9fqqdces1p51b',
+    host: 'ec2-54-225-72-238.compute-1.amazonaws.com',
+    user: 'gbrmmmxlvzbwlb',
+    password: '8c344f13f7166457b94caefbb9340bf599ba7f3dcb26bc95bd91dcdbf7349316',
+    port: 5432
 })
 
 client.connect()
